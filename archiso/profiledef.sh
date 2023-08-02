f8fc34dc9805e51f16b0665e462a72341d2f865f
Alessio <alessio@linux.com>
2023-08-02 18:37:07 +0200

    kde settings

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -19,9 +19,6 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/fix-keys"]="0:0:755"
 )
